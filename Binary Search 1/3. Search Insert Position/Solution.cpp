76ccc27568d570ac0872f55177c9b447536771e6
EsEz1 <ezzateslam131@gmail.com>
2023-01-17 23:51:29 +0200

    Binary Search |

---
+++ Binary Search 1/3. Search Insert Position/Solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start=0;
+        int end=nums.size()-1;
+        int idx=-1;
+        while(start<=end)
+        {
+            int mid=(start+end)>>1;
+            if(nums[mid]==target)
+            {
+                idx=mid;
+                break;
+            }
+            else if(nums[mid]>target)
+            {
+                end=mid-1;
+            }
+            else
+            {
+                start=mid+1;
+            }
+        }
+        if(idx==-1)
+        {
+            return end+1;
+        }
+        else
+        {
+            return idx;
+        }
+    }
+};
